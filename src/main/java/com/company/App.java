74a5013dd3b889001c6477b16066a12a90030d0f
Draxawox <kalix321@gmail.com>
2021-09-16 11:25:03 +0200

    pom updated

---
+++ src/main/java/com/company/App.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    public static void main(String[] args) {
+        var logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello world");
+    }
+}
---
+++ src/test/java/com/company/AppTest.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest {
+    @Test
+    public void test() throws Exception {
+        assertTrue(true);
+    }
+
+
+}
